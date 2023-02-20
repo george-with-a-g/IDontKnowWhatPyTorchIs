e9af862c718ace4a201842975098d9df45e3a75f
george-with-a-g <apsgeorge6@gmail.com>
2023-02-21 01:55:41 +0300

    Got an understanding of building models in PyTorch

--- index.py
+++ index.py
@@ -122,3 +122,59 @@ def aSimpleRecurrentNeuralNetwork():
     '''
 
 
+'''
+BUILDING MODELS IN PYTORCH
+'''
+def initializeModel():
+    import torch.nn as nn#Contains the neural network layers that we'll componse into the model
+    import torch.nn.functional as F#Activation functions
+    '''
+    Lenet5 - earliest convulutional neural networks.
+        Built to read small images of hand-written numbers and correctly classify which digit
+        was represented in the image.
+    Below we'll express Lenet5 in code.
+    '''
+    class LeNet(nn.Module):
+        def __init__(self):
+            super(LeNet, self).__init__()
+            # 1 input image channel (black & white), 6 output channels, 3x3 square convolution
+            # kernel
+            self.conv1 = nn.Conv2d(1, 6, 3)
+            self.conv2 = nn.Conv2d(6, 16, 3)
+            # an affine operation: y = Wx + b
+            self.fc1 = nn.Linear(16 * 6 * 6, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, 10)
+
+        def forward(self, x):
+            # Max pooling over a (2, 2) window
+            x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+            # If the size is a square you can only specify a single number 
+            x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+            x = x.view(-1, self.num_flat_features(x))
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+        def num_flat_features(self, x):
+            size = x.size()[1:]#all dimensions except the batch dimension
+            num_features = 1
+            for s in size:
+                num_features *= s
+            return num_features
+
+    net = LeNet()
+    print(net)# what the object tells us about itself
+    
+    input = torch.rand(1, 1, 32, 32) # stand-in for a 32x32 black & white image
+    print('\nImage batch shape')
+    print(input.shape)
+
+    output = net(input) #We don't call forward() directly
+    print('\nRaw output:')
+    print(output)
+    print(output.shape)
+
+
+#initializeModel()
